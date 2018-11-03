e26ab49275708959735fa1cb98bdedd43fc265f1
Shannon B. Barnes <shannon.barnes@oath.com>
2018-11-02 23:19:38 -0700

    attempt

--- src/main/scala/TwitterPipeline.scala
+++ src/main/scala/TwitterPipeline.scala
@@ -2,12 +2,17 @@ import org.http4s._
 import org.http4s.client.blaze._
 import org.http4s.client.oauth1
 import cats.effect._
-import fs2.Stream
+import fs2.{Chunk, Pure, Sink, Stream}
 import jawnfs2._
 import com.typesafe.config.ConfigFactory
+import fs2.concurrent.Queue
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.global
+
+case class State(count: Int)
+
 
 class TwitterPipeline(implicit t: Timer[IO]) {
 
@@ -26,6 +31,37 @@ class TwitterPipeline(implicit t: Timer[IO]) {
   private val req = Request[IO](Method.GET, Uri.uri("https://stream.twitter.com/1.1/statuses/sample.json"))
   private val collectDuration = 1.seconds
 
+  private val emptyState = State(0)
+
+
+
+
+  val queue = Queue.synchronous[IO,State]
+
+  def fromQueue: IO[State] = {
+    queue.flatMap{ q =>
+      println("from queue")
+      val v = q.dequeue1
+      println("dequeed")
+      v
+    }
+  }
+
+  val stateStream: Stream[IO, State] = Stream.eval(IO(emptyState)) ++ Stream.repeatEval(fromQueue)
+
+  def stateSink(s: Stream[IO, State]): Stream[IO, Unit] = {
+    println("am I ever here!!!!")
+    s.map { newState =>
+      println("OR here!!!")
+      println(newState)
+      queue.map { q =>
+        println("new State inserted")
+        q.enqueue1(newState)
+      }
+    }
+  }
+
+
   private def authenticate: IO[Request[IO]] = {
     oauth1.signRequest(
       req,
@@ -34,6 +70,8 @@ class TwitterPipeline(implicit t: Timer[IO]) {
       verifier = None,
       token = Some(oauth1.Token(accessToken, accessTokenSecret)))
   }
+
+
 
   protected def source: Stream[IO, TwitterObject] = {
     val source = for {
@@ -51,10 +89,16 @@ class TwitterPipeline(implicit t: Timer[IO]) {
   }
 
   def tweetStream: Stream[IO, Unit] = {
-    source
-      .groupWithin(Int.MaxValue, collectDuration)
-      .to(InMemoryDataStore)
-  }
 
+      source
+        .groupWithin(Int.MaxValue, collectDuration)
+        .zip(stateStream)
+        .map {
+         case (chunk, s) =>
+           println(s)
+           s.copy(count = s.count + 1)
+        }
+        .to(stateSink)
+    }
 }
 
